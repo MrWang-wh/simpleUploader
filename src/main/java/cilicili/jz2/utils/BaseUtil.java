c6ea18fdb3ab8444cd52aa13dba89f378e5c6ef7
Wanghan <316213301@qq.com>
2021-01-01 21:57:05 +0800

    1

--- src/main/java/cilicili/jz2/utils/BaseUtil.java
+++ src/main/java/cilicili/jz2/utils/BaseUtil.java
@@ -1,6 +1,6 @@
 package cilicili.jz2.utils;
 
 public class BaseUtil {
-	public static final String STORAGE_DIR = "D:/ideaworkspace/cilicili-master/storage_server/storage/";
+	public static final String STORAGE_DIR = "D:/ideaworkspace/storage_server/storage/";
 	public static final long MAX_FILE_SIZE = 52428800L;
 }
